c5b52ff4e96742cd9feff8117876120792d9d277
LTurns <lizzieturney@gmail.com>
2020-05-13 16:33:04 +0100

    business logic

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,16 +1,36 @@
 class Thermostat{
   constructor() {
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.MINIMUM_TEMPERATURE = 10;
-    this.temperature = 20;
+    this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
   }
 
   getCurrentTemperature(){
     return this.temperature;
   }
 
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+}
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
   up() {
-   this.temperature += 1
- }
+  if (this.isMaximumTemperature()) {
+    return;
+  }
+  this.temperature += 1;
+}
 
  down() {
    if (this.isMinimumTemperature()) {
@@ -22,5 +42,26 @@ class Thermostat{
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
+
+  isMaximumTemperature() {
+   if (this.isPowerSavingModeOn() === false) {
+     return this.temperature === this.MAX_LIMIT_PSM_OFF;
+   }
+   return this.temperature === this.MAX_LIMIT_PSM_ON;
+ }
+
+ resetTemperature() {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+}
+
+energyUsage() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+}
 
   };
