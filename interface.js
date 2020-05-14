3ca06276a6f425ef8746dffe61a82e144e23c0b2
Jed Stanton <jedstanton@hotmail.com>
2020-05-14 16:03:56 +0100

    Weather API added

--- interface.js
+++ interface.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   }
 
+  function displayWeather(city) {
+     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+     var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+     var units = '&units=metric';
+     $.get(url + token + units, function(data) {
+       $('#current-temperature').text(data.main.temp);
+     })
+   }
+
   updateTemperature();
 
   $('#temperature-up').on('click', function(){
@@ -31,4 +40,12 @@ $(document).ready(function() {
     thermostat.switchPowerSavingModeOff();
     $('#power-saving-status').text('OFF');
   });
+
+   displayWeather('London');
+
+  $('#select-city').submit(function(event){
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
 });
