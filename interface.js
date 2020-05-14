be84a443d8c27264708620b47b2fc5ccce86087c
mbrad26 <mbrad26@gmail.com>
2020-05-14 15:07:13 +0100

    implemented interface

--- interface.js
+++ interface.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   function updateTemperature(){
     $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
 
   updateTemperature();
@@ -15,4 +16,19 @@ $(document).ready(function() {
     thermostat.down();
     updateTemperature();
   })
-})
+
+  $('#temperature-reset').on('click', function () {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').on('click', function () {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('ON');
+  });
+
+  $('#powersaving-off').on('click', function () {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('OFF');
+  });
+});
